4eff1c68020b4e1d12a5d847444ce8b9961c9af5
Aashik81443 <56648807+Aashik81443@users.noreply.github.com>
2023-09-17 00:09:52 +0545

    minor css updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const apiKey = '33db9b1e88366d1c114a3d23c45102cc';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon')
 const defaultCity = 'Nepal';
 
 async function checkWeather(city) {
@@ -11,11 +12,15 @@ async function checkWeather(city) {
     }
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
-    
+
     document.querySelector('.city').innerHTML = data?.name;
     document.querySelector('.temp').innerHTML = Math.round(data?.main?.temp);
     document.querySelector('.humidity-value').innerHTML = data?.main?.humidity;
     document.querySelector('.wind-value').innerHTML = data?.wind?.speed;
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "./assets/images/clouds.png";
+    }
 }
 
 checkWeather(defaultCity);
