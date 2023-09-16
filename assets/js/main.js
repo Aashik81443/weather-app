fe0ad775ca28f214b5f226d1e13382aa1dffaa29
Aashik81443 <56648807+Aashik81443@users.noreply.github.com>
2023-09-16 22:57:45 +0545

    weather files added

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+const apiKey = '33db9b1e88366d1c114a3d23c45102cc';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const defaultCity = 'Nepal';
+
+async function checkWeather(city) {
+    if (!city) {
+        city = defaultCity;
+    }
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    let data = await response.json();
+    
+    document.querySelector('.city').innerHTML = data?.name;
+    document.querySelector('.temp').innerHTML = Math.round(data?.main?.temp);
+    document.querySelector('.humidity-value').innerHTML = data?.main?.humidity;
+    document.querySelector('.wind-value').innerHTML = data?.wind?.speed;
+}
+
+checkWeather(defaultCity);
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+
+})
