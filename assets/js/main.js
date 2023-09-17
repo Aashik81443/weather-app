3be6bcd3b6daca8fad8460d5b25d3690ea66ade0
Aashik81443 <56648807+Aashik81443@users.noreply.github.com>
2023-09-17 19:30:43 +0545

    js updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const apiKey = '33db9b1e88366d1c114a3d23c45102cc';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('.weather-icon')
+const weatherIcon = document.querySelector('.Weather-icon')
 const defaultCity = 'Nepal';
 
 async function checkWeather(city) {
@@ -13,14 +13,30 @@ async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
 
-    document.querySelector('.city').innerHTML = data?.name;
-    document.querySelector('.temp').innerHTML = Math.round(data?.main?.temp);
-    document.querySelector('.humidity-value').innerHTML = data?.main?.humidity;
-    document.querySelector('.wind-value').innerHTML = data?.wind?.speed;
 
-    if (data.weather[0].main == "Clouds") {
-        weatherIcon.src = "./assets/images/clouds.png";
+    if (response.status == 404) {
+        alert('Please Enter valid city name')
+    } else {
+
+        document.querySelector('.city').innerHTML = data?.name;
+        document.querySelector('.temp').innerHTML = Math.round(data?.main?.temp);
+        document.querySelector('.humidity-value').innerHTML = data?.main?.humidity;
+        document.querySelector('.wind-value').innerHTML = data?.wind?.speed;
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "./assets/images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "./assets/images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "./assets/images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "./assets/images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "./assets/images/mist.png";
+        }
     }
+
+
 }
 
 checkWeather(defaultCity);
